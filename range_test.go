b4d115fee3e6e6f7ba15fee3aad9bf79533c1aff
Juan Jose Medina <juanjo@bitnami.com>
2017-05-02 12:59:53 +0200

    Support the ~> operator as an alias to ~ (#11)

--- range_test.go
+++ range_test.go
@@ -1,6 +1,51 @@
 package semver
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+// The tilde is not actually included here, so we can prefix the alternative forms: ~ or ~>
+var tildeTests = map[string]map[string]bool{
+	"1.3.4": {
+		"0.0.1":    false,
+		"1.3.4":    true,
+		"1.1.0":    false,
+		"1.3.3":    false,
+		"1.3.5":    true,
+		"1.3.9999": true,
+		"1.4.0":    false,
+		"1.6.7":    false,
+		"1.13.0":   false,
+		"2.0.0":    false,
+	},
+	"1.3": {
+		"0.0.1":    false,
+		"1.3.0":    true,
+		"1.3.4":    true,
+		"1.1.0":    false,
+		"1.3.3":    true,
+		"1.3.5":    true,
+		"1.3.9999": true,
+		"1.4.0":    false,
+		"1.6.7":    false,
+		"1.13.0":   false,
+		"2.0.0":    false,
+	},
+	"1": {
+		"0.0.1":    false,
+		"1.3.0":    true,
+		"1.3.4":    true,
+		"1.1.0":    true,
+		"1.3.3":    true,
+		"1.3.5":    true,
+		"1.3.9999": true,
+		"1.4.0":    true,
+		"1.6.7":    true,
+		"1.13.0":   true,
+		"2.0.0":    false,
+	},
+}
 
 var rangeTestBattery = map[string]map[string]bool{
 	"^1.3.4": {
@@ -25,44 +70,6 @@ var rangeTestBattery = map[string]map[string]bool{
 		"0.1.0":   false,
 		"0.0.2":   true,
 		"0.0.999": false,
-	},
-	"~1.3.4": {
-		"0.0.1":    false,
-		"1.3.4":    true,
-		"1.1.0":    false,
-		"1.3.3":    false,
-		"1.3.5":    true,
-		"1.3.9999": true,
-		"1.4.0":    false,
-		"1.6.7":    false,
-		"1.13.0":   false,
-		"2.0.0":    false,
-	},
-	"~1.3": {
-		"0.0.1":    false,
-		"1.3.0":    true,
-		"1.3.4":    true,
-		"1.1.0":    false,
-		"1.3.3":    true,
-		"1.3.5":    true,
-		"1.3.9999": true,
-		"1.4.0":    false,
-		"1.6.7":    false,
-		"1.13.0":   false,
-		"2.0.0":    false,
-	},
-	"~1": {
-		"0.0.1":    false,
-		"1.3.0":    true,
-		"1.3.4":    true,
-		"1.1.0":    true,
-		"1.3.3":    true,
-		"1.3.5":    true,
-		"1.3.9999": true,
-		"1.4.0":    true,
-		"1.6.7":    true,
-		"1.13.0":   true,
-		"2.0.0":    false,
 	},
 	"=1.3.4": {
 		"1.3.4": true,
@@ -165,27 +172,28 @@ var rangeTestBattery = map[string]map[string]bool{
 	},
 }
 
-func TestRangeOperator(t *testing.T) {
-	for rangeStr, data := range rangeTestBattery {
-		r := MustParseRange(rangeStr)
-		for v, result := range data {
-			if r.Contains(MustParseVersion(v)) != result {
-				t.Errorf("Expected %v of %v to evaluate to %v", rangeStr, v, result)
-			}
+func testRange(t *testing.T, rangeStr string, battery map[string]bool) {
+	r := MustParseRange(rangeStr)
+	re := r.RegExp()
+	for v, result := range battery {
+		nv := MustParseVersion(v)
+		if r.Contains(nv) != result {
+			t.Errorf("Expected %v of %v to evaluate to %v", rangeStr, v, result)
+		}
+		if re[0].MatchString(nv.String()) &&
+			re[1].MatchString(nv.String()) != result {
+			t.Errorf("Expected %v of %v to evaluate to %v (Using regexp checks)", rangeStr, v, result)
 		}
 	}
 }
 
-func TestRangeOperatorRe(t *testing.T) {
+func TestRangeOperator(t *testing.T) {
 	for rangeStr, data := range rangeTestBattery {
-		r := MustParseRange(rangeStr)
-		re := r.RegExp()
-		for v, result := range data {
-			nv := MustParseVersion(v)
-			if re[0].MatchString(nv.String()) &&
-				re[1].MatchString(nv.String()) != result {
-				t.Errorf("Expected %v of %v to evaluate to %v", rangeStr, v, result)
-			}
+		testRange(t, rangeStr, data)
+	}
+	for rangeStr, data := range tildeTests {
+		for _, op := range []string{"~", "~>"} {
+			testRange(t, fmt.Sprintf("%s%s", op, rangeStr), data)
 		}
 	}
 }
